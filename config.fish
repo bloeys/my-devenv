965b3de200b816e35ea764ce8399c514a93f5a00
Omar Mahdi <bloeys@outlook.com>
2021-11-12 11:24:20 +0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,13 +2,17 @@ set fish_greeting
 
 if not ps -a | grep ssh-agent>/dev/null
 
-  sleep (math (random 5 5000) / 2500)
-  if not ps -a | grep ssh-agent>/dev/null
+  if not test -e /cygdrive/c/Users/user/.config/fish/myFishConfigLock
+    touch /cygdrive/c/Users/user/.config/fish/myFishConfigLock
+
     eval (ssh-agent -c)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 
     ssh-add ~/.ssh/id_rsa
+    ssh-add ~/.ssh/*.key
+
+    rm /cygdrive/c/Users/user/.config/fish/myFishConfigLock
   end
 end
 
@@ -17,4 +21,8 @@ alias gcp='git commit -a; and git push'
 alias gd='git diff'
 alias gs='git status'
 alias gobr='go build -o o.exe .; and ./o.exe; and rm ./o.exe'
-alias python='python3'
+alias ll='ls -al'
+alias got='gotest -count=1 ./...'
+alias python='/cygdrive/c/Users/user/AppData/Local/Programs/Python/Python37/python.exe'
+alias python3='/cygdrive/c/Users/user/AppData/Local/Programs/Python/Python37/python.exe'
+alias swig='/cygdrive/d/Program\ Files/swigwin-4.0.2/swig.exe'
